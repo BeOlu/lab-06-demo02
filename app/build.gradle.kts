661ca3b2f2c79b8467b75af7a8143e3d2f02f062
BeOlu <oluwabiy@ualberta.ca>
2026-02-12 11:06:32 -0700

    added javadocs to City class.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(files("/Users/macbook-rebecca/Library/Android/sdk/platforms/android-36/android.jar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/listycity/City.java
+++ app/src/main/java/com/example/listycity/City.java
@@ -19,11 +19,25 @@ public class City implements Comparable<City> {
         return this.province;
     }
 
+    /**
+     * This is a method that compares two cities based on their names.
+     * @param o
+     * This is the other city to compare to
+     * @return
+     * Returns a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object.
+     */
     @Override
     public int compareTo(City o) {
         return this.city.compareTo(o.getCityName()); // this.city refers to the city name
     }
 
+
+    /**
+     *
+     * @param o   the reference object with which to compare.
+     * @return
+     * Returns true if the two objects are equal, false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -32,6 +46,12 @@ public class City implements Comparable<City> {
         return Objects.equals(city, city1.city) && Objects.equals(province, city1.province);
     }
 
+
+    /**
+     * This is a method that returns a hash code value for the object.
+     * @return
+     * Returns a hash code value for this object.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(city, province);
